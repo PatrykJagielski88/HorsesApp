a62229b4ce83c250400daf557d130451abe0a0dd
Patryk Jagielski <pjagielski@academic.rrc.ca>
2022-10-28 14:33:57 -0500

    image magic is working

--- app/admin/horses.rb
+++ app/admin/horses.rb
@@ -1,11 +1,19 @@
 ActiveAdmin.register Horse do
-
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  permit_params :name, :age, :top_speed, :breed_id, :number_of_legs
+  permit_params :name, :age, :top_speed, :breed_id, :number_of_legs, :image
+
+  form do |f|
+    f.semantic_errors
+    f.inputs
+    f.inputs do
+      f.input :image, as: :file, hint: f.object.image.present? ? image_tag(f.object.image, size: '50x50') : ''
+    end
+    f.actions
+  end
   #
   # or
   #
@@ -14,5 +22,4 @@ ActiveAdmin.register Horse do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
 end
